19a8c5fd0bcd027d3a5dd39c1a36d3e77efe4078
Jateen Joharapurkar <jjateen19@gmail.com>
2024-10-15 11:29:10 -0700

    Added FourBitAdder.h with proper notation

--- source/FourBitAdder/FourBitAdder.h
+++ source/FourBitAdder/FourBitAdder.h
@@ -2,3 +2,23 @@
 
 // Note that you will need intermediate classes as well
 // for the half-adder and full-adder circuits.
+#ifndef FOURBITADDER_H
+#define FOURBITADDER_H
+
+#include "intern/FullAdder.h"
+#include <string>
+#include <vector>
+
+class FourBitAdder
+{
+public:
+    FourBitAdder();
+    std::string add(const std::string& strBinaryA, const std::string& strBinaryB, int iCarry);
+
+private:
+    void connectAdders();
+    FullAdder m_fullAdders[4];
+};
+
+#endif // FOURBITADDER_H
+
