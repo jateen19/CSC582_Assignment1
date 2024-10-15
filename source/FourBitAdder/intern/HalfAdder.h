aff4bd0f778edd9f05fbd2a7cd4bd1cebfc05e5a
Jateen Joharapurkar <jjateen19@gmail.com>
2024-10-15 09:58:32 -0700

    Implemented HalfAdder.h and ran astyle on it

---
+++ source/FourBitAdder/intern/HalfAdder.h
@@ -0,0 +1,29 @@
+#ifndef HALFADDER_H
+#define HALFADDER_H
+
+#include "LogicGates.h"
+
+class HalfAdder {
+public:
+    HalfAdder();
+    ~HalfAdder();
+
+    // Set inputs A and B for the half adder
+    void setInputs(const unsigned short int a, const unsigned short int b);
+
+    // Get the calculated sum (A ⊕ B)
+    unsigned short int getSum() const;
+
+    // Get the calculated carry (A ∧ B)
+    unsigned short int getCarry() const;
+
+private:
+    AND m_andGate;   // For calculating the carry
+    OR m_orGate;     // Used with NAND to calculate XOR (sum)
+    NAND m_nandGate; // Used with OR to calculate XOR (sum)
+
+    unsigned short int m_sum;   // Holds the result of the sum
+    unsigned short int m_carry; // Holds the result of the carry
+};
+
+#endif  // HALFADDER_H
